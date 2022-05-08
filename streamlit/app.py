523240f55b67cc2bb48d3b98fee2040fc02d4d58
unknown <marktraquair@gmail.com>
2022-05-08 10:10:50 -0400

    Corrections to streamlit app, as well as addition of the rest of the charts

--- streamlit/app.py
+++ streamlit/app.py
@@ -18,19 +18,26 @@ def parse_timestamps(time_string: str) -> int:
 
 @st.cache
 def get_data(path: str = "../data/YYZ.csv"):
-    source = pd.read_csv(path)
-    source = source.applymap(parse_timestamps)
+    source = pd.read_csv(path, index_col=0)
+    source = source.applymap(parse_timestamps) 
     source = source.reset_index()
+    source["date"] = source["index"]
+    source = source.drop("index", axis=1)
+    source["date"] = pd.to_datetime(source["date"])
     return source
 
 source = get_data()
 
 # Original time series chart. Omitted `get_chart` for clarity
-chart = alt.Chart(source).mark_line().encode(
-    x='index:T',
-    y='T1 International:Q'
-)
+charts = []
+for column in source.drop("date",axis=1):
+    chart = alt.Chart(source).mark_line().encode(
+        x='date:T',
+        y=f'{column}:Q'
+    )
+    charts.append(chart)
 
 # Display both charts together
 st.title("Wait time trends at YYZ")
-st.altair_chart(chart.interactive(), use_container_width=True)
+for chart in charts:
+    st.altair_chart(chart.interactive(), use_container_width=True)
